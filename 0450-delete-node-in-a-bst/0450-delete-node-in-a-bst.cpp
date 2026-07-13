806d338794a4ba9adab4add429e9f04603bb1e22
bhardwajsaurav1 <sauravbhardwaj052@gmail.com>
2026-07-13 16:37:39 +0530

    Time: 0 ms (100%), Space: 34.5 MB (15.58%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,64 +11,33 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        TreeNode* temp=root;
-        TreeNode* prev=NULL;
-        while(temp&& temp->val!=key){
-            prev=temp;
-            temp=(temp->val<key)?temp->right:temp->left;
-        }
-        if(temp==NULL) return root;// it means there is no key to delete
-        // case 1 if the key element is leaf
-        if(!temp->left&&!temp->right){
-           if(prev==NULL){// means we are deleting the root node
-                delete(temp);
+    TreeNode* helper(TreeNode* root, int key){
+        if(root==NULL) return NULL;
+        if(root->val==key){
+            // case 1 : leaf node
+            if(!root->left&& !root->right){
                 return NULL;
-           } 
-           if(prev->left==temp) prev->left=NULL;
-           else prev->right=NULL;
-           delete(temp);
-
-        }
-        // case 2 two child
-        else if(temp->left&&temp->right){
-            TreeNode* curr=temp->left;
-            while(curr->right) curr=curr->right;
-            curr->right=temp->right;
-            if(prev==NULL){// means we are deleting the head
-               TreeNode* newR=temp->left;
-                delete temp;
-                return newR;
             }
-            else {
-                
-                if(prev->left==temp) prev->left=temp->left;
-                else prev->right=temp->left;
-                delete temp;
+            // case 2: two child
+            else if(root->left&& root->right){
+                TreeNode* temp=root->left;
+                while(temp->right) temp=temp->right;
+                temp->right=root->right;
+                return root->left;
+            }
+            else{
+                if(root->left) return root->left;
+                else return root->right;
             }
         }
-        else { //only one child
-            if(temp->left){
-                if(prev==NULL) {
-                    TreeNode* newR= temp->left;
-                    delete temp;
-                    return newR;
-                }
-                else if(prev->left==temp) prev->left=temp->left;
-                else prev->right=temp->left;
-                delete temp;
-            }
-            else {
-                if(prev==NULL) {
-                    TreeNode* newR= temp->right;
-                    delete temp;
-                    return newR;
-                }
-                else if(prev->left==temp) prev->left=temp->right;
-                else prev->right=temp->right;
-                delete temp;
-            }
+        if(root->val>key){
+            root->left=helper(root->left,key);
         }
+        else root->right=helper(root->right,key);
         return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return NULL;
+        return helper(root,key);
     }
 };
