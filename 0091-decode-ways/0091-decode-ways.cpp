699930e43fcdc68549575b012be675f779b3779b
bhardwajsaurav1 <sauravbhardwaj052@gmail.com>
2026-08-02 16:08:40 +0530

    Time: 2 ms (21.9%), Space: 8.5 MB (80.05%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int ans=0;
-    int dp[100];
+    int dp[101];
     int solve(int idx,string&s, int n){
-        if(idx==n) return 1;
-        if(idx>n) return 0;
-        if(s[idx]=='0') return 0;
+        if(idx==n) return dp[idx]=1;
+        if(s[idx]=='0') return dp[idx]=0;
+
         if(dp[idx]!=-1) return dp[idx];
-        ans+= solve(idx+1,s, n);
-        int num=stoi(s.substr(idx,2));
-        if(num<=26){
+        int ans= solve(idx+1,s, n);
+        if(idx+1<n){
+            if(s[idx]=='1'||s[idx]=='2'&&s[idx+1]<='6'){
             ans+=solve(idx+2,s,n);
+            }
         }
-        dp[idx]=ans;
-        return 0;
+        return dp[idx]=ans;
+        
     }
     int numDecodings(string s) {
         memset(dp,-1,sizeof(dp));
-        solve(0,s,s.size());
-        return ans;
+        return solve(0,s,s.size());
     }
 };
