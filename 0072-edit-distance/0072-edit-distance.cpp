b965feee19a86d7227d5804399ad9a29b8856ced
bhardwajsaurav1 <sauravbhardwaj052@gmail.com>
2026-08-06 17:51:56 +0530

    Time: 105 ms (5.1%), Space: 80.7 MB (9.32%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int dp[501][501];
-    int solve(string s, string t, int i, int j){
-        if(i==s.size()){
-            return dp[i][j]=t.size()-j;
+    int solve(string s, string t, int m, int n){
+        if(m==0){
+            return n;
         }
-        if(j==t.size()){
-            return dp[i][j]=s.size()-i;
+        else if(n==0){
+            return m;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]=solve(s, t, i+1,j+1);
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s[m-1]==t[n-1]){
+            return dp[m][n]=solve(s, t, m-1,n-1);
         }
-        int insert=1+solve(s,t,i,j+1);
-        int del=1+solve(s,t,i+1,j);
-        int replace=1+solve(s,t,i+1,j+1);
-        return dp[i][j]=min({insert,del,replace});
+        int insert=1+solve(s,t,m,n-1);
+        int del=1+solve(s,t,m-1,n);
+        int replace=1+solve(s,t,m-1,n-1);
+        return dp[m][n]=min({insert,del,replace});
     }
     int minDistance(string word1, string word2) {
         memset(dp,-1, sizeof(dp));
-        return solve(word1,word2,0,0);
+        return solve(word1,word2,word1.size(),word2.size());
     }
 };
