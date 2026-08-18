e589391f1a0d358c5527773d2a8cbbaa20d1a369
bhardwajsaurav1 <sauravbhardwaj052@gmail.com>
2026-08-18 16:07:09 +0530

    Time: 187 ms (5.09%), Space: 74.9 MB (5.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
+    vector<vector<int>>direction={{0,1},{0,-1},{1,0},{-1,0}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        // Fast I/O
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-
-        int m = mat.size();
-        int n = mat[0].size();
-        queue<pair<int, int>> qt;
-
-        // Step 1: Enqueue all 0s and mark 1s as -1 (unvisited)
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    qt.push({i, j});
-                } else {
-                    mat[i][j] = -1; 
+        int m=mat.size();
+        int n=mat[0].size();
+        //vector<vector<int>>ans(m,vector<int>(n));
+        queue<pair<int,int>>qt;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                qt.push({i,j});
+                }
+                else mat[i][j]=-1;
+            }
+        }
+        while(!qt.empty()){
+            int size=qt.size();
+            for(int i=0;i<size;i++){
+                int curr_i=qt.front().first;
+                int curr_j=qt.front().second;
+                qt.pop();
+                for(vector<int> dir:direction){
+                    int new_i=curr_i+dir[0];
+                    int new_j=curr_j+dir[1];
+                    if(new_i>=0&&new_i<m&&new_j>=0&&new_j<n&&mat[new_i][new_j]==-1){
+                        mat[new_i][new_j]=mat[curr_i][curr_j]+1;
+                        qt.push({new_i,new_j});
+                    }
                 }
             }
         }
-
-        // Direction arrays avoid heap allocations
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-
-        // Step 2: Multi-source BFS
-        while (!qt.empty()) {
-            auto [r, c] = qt.front();
-            qt.pop();
-
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dx[k];
-                int nc = c + dy[k];
-
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1) {
-                    mat[nr][nc] = mat[r][c] + 1;
-                    qt.push({nr, nc});
-                }
-            }
-        }
-
         return mat;
     }
 };
